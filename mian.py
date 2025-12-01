4bfd7d2fc1f96bca1a6ff0e4a491a5861dd10bb3
suarez-26 <santiago.suarezh26@gmail.com>
2025-12-01 08:59:04 -0500

    feat: :sparkles: Iniciando en el Proyecto

---
+++ mian.py
@@ -0,0 +1,10 @@
+from utils.menus import *
+
+def Registrar_Gasto():
+    cantidad = float(input("Ingrese el monto :"))
+    categoria = input("Ingrese la categoria :")
+    descripcion = input("Ingrese una breve descripcion (opcional) :")
+    
+    
+
+Menu_Principal()
